aacf0c5303a6c221598de3bf80f28f243d1817ad
antrivera <anthony.rivs@gmail.com>
2016-11-11 14:47:19 -0800

    API endpoints

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'json'
+require './lib/app_helper.rb'
+
+before %r{/(.+)} do
+  content_type :json
+end
 
 get '/' do
-  "It works"
+  erb :index
+end
+
+get '/emails' do
+  num_unique = params["unique"].to_i
+  emails = create_email_list(num_unique).to_json
+end
+
+post '/unique_emails' do
+  json = JSON.parse(request.body.read, symbolize_names: true)
+  remove_duplicates(json[:emails]).to_json
 end
